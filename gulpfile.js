ee353138add749e90f892792038f2e98d9066005
Sean Denny <nektromc@gmail.com>
2018-06-07 18:00:45 -0400

    gulpfile - put strings in quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
-const gulp = require('gulp');
-const gih = require('gulp-include-html');
-const htmlmin = require('gulp-htmlmin');
+const gulp = require("gulp");
+const gih = require("gulp-include-html");
+const htmlmin = require("gulp-htmlmin");
 
-gulp.task('html', function() {
-    return gulp.src('./src/**/*.html')
+gulp.task("html", function() {
+    return gulp.src("./src/**/*.html")
     .pipe(gih())
     //.pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('bin'));
+    .pipe(gulp.dest("bin"));
 });
 
-gulp.task('static', function() {
-    return gulp.src(['./static/**/*.*'])
-    .pipe(gulp.dest('bin'));
+gulp.task("static", function() {
+    return gulp.src(["./static/**/*.*"])
+    .pipe(gulp.dest("bin"));
 });
 
-gulp.task('static-2', function() {
-    return gulp.src(['./src/**/*.{png,ico,txt,c,bmp,ttf,js,css,mp3,yml,json}'])
-    .pipe(gulp.dest('bin'));
+gulp.task("static-2", function() {
+    return gulp.src(["./src/**/*.{png,ico,txt,c,bmp,ttf,js,css,mp3,yml,json}"])
+    .pipe(gulp.dest("bin"));
 });
 
-gulp.task('netlify', function() {
-    return gulp.src('./static/_headers').pipe(gulp.dest('bin'));
+gulp.task("netlify", function() {
+    return gulp.src("./static/_headers").pipe(gulp.dest("bin"));
 });
 
-gulp.task('site-dev', [
-    'html',
-    'static-2',
-])
+gulp.task("site-dev", [
+    "html",
+    "static-2",
+]);
 
-gulp.task('default', [
-    'site-dev',
-    'static',
-    'netlify',
+gulp.task("default", [
+    "site-dev",
+    "static",
+    "netlify",
 ]);
